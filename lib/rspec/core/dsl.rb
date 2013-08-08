ccbf9cb7591212de881c916749e5eea7276d12a0
Jon Rowe <hello@jonrowe.co.uk>
2013-08-08 15:42:24 +1000

    top level dsl under RSpec

--- lib/rspec/core/dsl.rb
+++ lib/rspec/core/dsl.rb
@@ -1,23 +1,23 @@
 module RSpec
+
+  # Generates a subclass of {ExampleGroup}
+  #
+  # ## Examples:
+  #
+  #     describe "something" do
+  #       it "does something" do
+  #         # example code goes here
+  #       end
+  #     end
+  #
+  # @see ExampleGroup
+  # @see ExampleGroup.describe
+  def self.describe(*args, &example_group_block)
+    RSpec::Core::ExampleGroup.describe(*args, &example_group_block).register
+  end
+
   module Core
-    # Adds the `describe` method to the top-level namespace.
     module DSL
-      # Generates a subclass of {ExampleGroup}
-      #
-      # ## Examples:
-      #
-      #     describe "something" do
-      #       it "does something" do
-      #         # example code goes here
-      #       end
-      #     end
-      #
-      # @see ExampleGroup
-      # @see ExampleGroup.describe
-      def describe(*args, &example_group_block)
-        RSpec::Core::ExampleGroup.describe(*args, &example_group_block).register
-      end
     end
   end
 end
-
